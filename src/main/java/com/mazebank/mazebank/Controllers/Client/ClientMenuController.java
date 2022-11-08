92bbc486a27e9ef6f382551753629e395898bd9b
ViniUK00 <elvin.bilo@icloud.com>
2022-11-08 13:02:32 +0000

    added ClientMenu.css ClientMenu.fxml ClientMenuController.java

---
+++ src/main/java/com/mazebank/mazebank/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,19 @@
+package com.mazebank.mazebank.Controllers.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    public Button dashboard_btn;
+    public Button transaction_btn;
+    public Button account_btn;
+    public Button profile_btn;
+    public Button logout_btn;
+    public Button report_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){}
+}
