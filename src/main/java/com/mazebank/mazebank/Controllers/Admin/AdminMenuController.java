30745ab5bb50d751d2c2349c598014c11b1f40f4
ViniUK00 <elvin.bilo@icloud.com>
2022-11-11 00:26:39 +0000

    added admin fxml,controller and modifies CSS

---
+++ src/main/java/com/mazebank/mazebank/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,17 @@
+package com.mazebank.mazebank.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    public Button create_client_btn;
+    public Button clients_btn;
+    public Button deposint_btn;
+    public Button logout_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+}
