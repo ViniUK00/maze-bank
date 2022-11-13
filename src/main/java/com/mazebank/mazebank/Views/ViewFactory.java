e74d3fa8ac62e5724e596439e1f8eba906078c6c
ViniUK00 <elvin.bilo@icloud.com>
2022-11-13 16:00:45 +0000

    added application icon and disabled window resize

--- src/main/java/com/mazebank/mazebank/Views/ViewFactory.java
+++ src/main/java/com/mazebank/mazebank/Views/ViewFactory.java
@@ -7,6 +7,7 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -149,6 +150,8 @@ public class ViewFactory {
         }
         Stage stage = new Stage();
         stage.setScene(scene);
+        stage.getIcons().add(new Image(String.valueOf(getClass().getResource("/Images/icon.png"))));
+        stage.setResizable(false);
         stage.setTitle("Maze Bank");
         stage.show();
     }
