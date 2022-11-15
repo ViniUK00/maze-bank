d295574e006698453f8275d76ea0cbf8187a3e81
ViniUK00 <elvin.bilo@icloud.com>
2022-11-15 19:33:36 +0000

    added account model (checking and savings account)

---
+++ src/main/java/com/mazebank/mazebank/Models/Account.java
@@ -0,0 +1,25 @@
+package com.mazebank.mazebank.Models;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public abstract class Account {
+    private final StringProperty owner;
+    private final StringProperty accountNumber;
+    private final DoubleProperty balance;
+
+    public Account(String owner, String accountNumber, double balance) {
+        this.owner = new SimpleStringProperty(this,"Owner", owner);
+        this.accountNumber = new SimpleStringProperty(this,  "Account Number", accountNumber);
+        this.balance = new SimpleDoubleProperty(this, "Balance", balance);
+    }
+
+    public StringProperty ownerProperty(){return owner;}
+
+    public StringProperty accountNumberProperty(){return accountNumber;}
+
+    public DoubleProperty balanceProperty(){return balance;}
+
+}
